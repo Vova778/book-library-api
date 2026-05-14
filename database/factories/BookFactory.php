ab575c9cd5fde0d23f917cee22dd3c5a207efb12
Volodymyr Korovai <voha.kopovay@gmail.com>
2026-05-14 13:49:13 +0300

    Add book factory and seeder

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'publisher' => fake()->company(),
+            'author' => fake()->name(),
+            'genre' => fake()->randomElement([
+                'Fantasy',
+                'Science Fiction',
+                'Drama',
+                'Mystery',
+                'Biography',
+                'History',
+                'Programming',
+            ]),
+            'publication_date' => fake()->dateTimeBetween('-50 years', 'now')->format('Y-m-d'),
+            'word_count' => fake()->numberBetween(30000, 250000),
+            'price_usd' => fake()->randomFloat(2, 5, 150),
+        ];
+    }
+}
