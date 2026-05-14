151bce73d7b9ef4af17e31bbe89221063517c7fc
Volodymyr Korovai <voha.kopovay@gmail.com>
2026-05-14 15:44:13 +0300

    Replace default welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => 'Book Library API',
+        'documentation_url' => url('/api/documentation'),
+    ], 200, [], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 });
